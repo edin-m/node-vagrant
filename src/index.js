9dbd166a0f303280bb8bc7739e68d8bccf7ed784
Edin Mujagic <edinmujagic2@gmail.com>
2017-08-10 22:27:53 +0200

    Use global status parser in globalStatus

--- src/index.js
+++ src/index.js
@@ -340,30 +340,7 @@ module.exports.globalStatus = function (args, cb) {
             return cb(err);
         }
 
-        var lines = out.split('\n').slice(2).reduce(function (prev, curr) {
-            if (prev.length > 0 && prev[prev.length - 1].length === 0) {
-                return prev;
-            }
-            prev.push(curr.trim());
-            return prev;
-        }, []);
-
-        lines.pop();
-        if (/no active Vagrant environments/.test(lines[0])) {
-            lines = [];
-        }
-
-        var re = /(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/;
-        lines = lines.map(function (line) {
-            var res = line.match(re);
-            return {
-                id: res[1],
-                name: res[2],
-                provider: res[3],
-                state: res[4],
-                cwd: res[5]
-            };
-        });
+        var lines = parsers.globalStatusParser(out);
 
         cb(null, lines);
     });
